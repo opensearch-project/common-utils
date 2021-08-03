6fec43e8a774f0e9b931334f4d3def0e50de76a4
Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
2021-08-03 09:45:42 -0700

    Adds tests for legacy destination response

--- src/test/java/org/opensearch/commons/destination/response/LegacyDestinationResponseTest.java
+++ src/test/java/org/opensearch/commons/destination/response/LegacyDestinationResponseTest.java
@@ -1,2 +1,65 @@
-package org.opensearch.commons.destination.response;public class LegacyDestinationResponseTest {
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.commons.destination.response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.opensearch.common.io.stream.BytesStreamOutput;
+import org.opensearch.common.io.stream.StreamInput;
+
+public class LegacyDestinationResponseTest {
+
+    @Test
+    public void testBuildingLegacyDestinationResponse() {
+        LegacyDestinationResponse res = new LegacyDestinationResponse.Builder()
+                .withStatusCode(200).withResponseContent("Hello world").build();
+
+        assertEquals(200, res.statusCode);
+        assertEquals("Hello world", res.getResponseContent());
+    }
+
+    @Test
+    public void testRoundTrippingLegacyDestinationResponse() throws IOException {
+        LegacyDestinationResponse res = new LegacyDestinationResponse.Builder()
+                .withStatusCode(200).withResponseContent("Hello world").build();
+        BytesStreamOutput out = new BytesStreamOutput();
+        res.writeTo(out);
+
+        StreamInput in = StreamInput.wrap(out.bytes().toBytesRef().bytes);
+        LegacyDestinationResponse newRes = new LegacyDestinationResponse(in);
+
+        assertEquals(res.statusCode, newRes.statusCode, "Round tripping doesn't work");
+        assertEquals(res.getResponseContent(), newRes.getResponseContent(), "Round tripping doesn't work");
+    }
+
+    @Test
+    public void testMissingLegacyDestinationResponse() {
+        try {
+            new LegacyDestinationResponse.Builder().withStatusCode(200).build();
+            fail("Creating LegacyDestinationResponse without response content should fail");
+        } catch (IllegalArgumentException ignored) {
+        }
+    }
+
+    @Test
+    public void testMissingLegacyDestinationStatusCode() {
+        try {
+            new LegacyDestinationResponse.Builder().withResponseContent("Hello world").build();
+            fail("Creating LegacyDestinationResponse without status code should fail");
+        } catch (IllegalArgumentException ignored) {
+        }
+    }
 }
