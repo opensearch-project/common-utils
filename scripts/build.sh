f7c78014e540f8da6fe821a6059e39f92b725a4a
Marc Handalian <handalm@amazon.com>
2021-11-04 07:16:42 -0700

    Update maven publication to include cksums. (#91)
    This change adds a local staging repo task that will include cksums.  It will also update build.sh to use this new task and copy the contents of the staging repo to the output directory.
    The maven publish plugin will not include these cksums when publishing to maven local but will when published to a separate folder.
    
    Signed-off-by: Marc Handalian <handalm@amazon.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -57,5 +57,6 @@ fi
 
 ./gradlew build -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT
 ./gradlew publishShadowPublicationToMavenLocal -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT
+./gradlew publishShadowPublicationToStagingRepository -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT
 mkdir -p $OUTPUT/maven/org/opensearch
-cp -r ./build/libs $OUTPUT/maven/org/opensearch/common-utils
+cp -r ./build/local-staging-repo/org/opensearch/common-utils $OUTPUT/maven/org/opensearch/common-utils
