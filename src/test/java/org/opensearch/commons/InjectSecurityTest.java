3919543651049284a877fdc0980aec76ced72a21
Daniel Doubrovkine (dB.) <dblock@dblock.org>
2022-03-01 17:09:12 -0500

    Run CI/CD on Java 8, 11, 14 and 17. (#121)
    * Run CI/CD on Java 8, 11, 14 and 17.
    
    Signed-off-by: dblock <dblock@dblock.org>
    
    * Add JDK 17.
    
    Signed-off-by: dblock <dblock@dblock.org>

--- src/test/java/org/opensearch/commons/InjectSecurityTest.java
+++ src/test/java/org/opensearch/commons/InjectSecurityTest.java
@@ -15,7 +15,7 @@ import static org.opensearch.commons.ConfigConstants.OPENSEARCH_SECURITY_INJECTE
 import static org.opensearch.commons.ConfigConstants.OPENSEARCH_SECURITY_USE_INJECTED_USER_FOR_PLUGINS;
 
 import java.util.Arrays;
-import java.util.Map;
+import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
 import org.opensearch.common.settings.Settings;
@@ -114,7 +114,11 @@ public class InjectSecurityTest {
             assertTrue(helper.injectProperty("property1", true));
             assertTrue(helper.injectProperty("property2", "some value"));
             assertTrue(helper.injectProperty("property3", ""));
-            assertTrue(helper.injectProperty("property4", Map.of("key", "value")));
+            assertTrue(helper.injectProperty("property4", new HashMap<String, String>() {
+                {
+                    put("key", "value");
+                }
+            }));
             // verify the set properties are not null and equal to what was set
             assertNull(threadContext.getTransient("property"));
             assertNotNull(threadContext.getTransient("property1"));
@@ -124,7 +128,11 @@ public class InjectSecurityTest {
             assertNotNull(threadContext.getTransient("property3"));
             assertEquals("", threadContext.getTransient("property3"));
             assertNotNull(threadContext.getTransient("property4"));
-            assertEquals(Map.of("key", "value"), threadContext.getTransient("property4"));
+            assertEquals(new HashMap<String, String>() {
+                {
+                    put("key", "value");
+                }
+            }, threadContext.getTransient("property4"));
         }
         assertEquals("1", threadContext.getHeader("default"));
         assertEquals("opendistro", threadContext.getHeader("name"));
