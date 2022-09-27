0691bb107fc50749481837ad11fcc46df7aae982
Petar Dzepina <petar.dzepina@vroom.com>
2022-09-28 00:31:48 +0200

    added JvmField annotation to prevent generating getters (#255)
    Signed-off-by: Petar Dzepina <petar.dzepina@vroom.com>
    (cherry picked from commit d64d992ab0a6e5ad92370e60fd3422608fc217bf)

--- src/main/kotlin/org/opensearch/commons/alerting/action/AlertingActions.kt
+++ src/main/kotlin/org/opensearch/commons/alerting/action/AlertingActions.kt
@@ -12,15 +12,16 @@ object AlertingActions {
     const val DELETE_MONITOR_ACTION_NAME = "cluster:admin/opendistro/alerting/monitor/delete"
     const val GET_FINDINGS_ACTION_NAME = "cluster:admin/opensearch/alerting/findings/get"
 
+    @JvmField
     val INDEX_MONITOR_ACTION_TYPE =
         ActionType(INDEX_MONITOR_ACTION_NAME, ::IndexMonitorResponse)
-
+    @JvmField
     val GET_ALERTS_ACTION_TYPE =
         ActionType(GET_ALERTS_ACTION_NAME, ::GetAlertsResponse)
-
+    @JvmField
     val DELETE_MONITOR_ACTION_TYPE =
         ActionType(DELETE_MONITOR_ACTION_NAME, ::DeleteMonitorResponse)
-
+    @JvmField
     val GET_FINDINGS_ACTION_TYPE =
         ActionType(GET_FINDINGS_ACTION_NAME, ::GetFindingsResponse)
 }
