514fa7acca686c6142ea847cfc1906398886fe41
Subhobrata Dey <sbcd90@gmail.com>
2023-04-19 01:10:07 +0000

    integrate security-analytics & alerting for correlation engine
    Signed-off-by: Subhobrata Dey <sbcd90@gmail.com>

--- src/main/kotlin/org/opensearch/commons/alerting/action/AlertingActions.kt
+++ src/main/kotlin/org/opensearch/commons/alerting/action/AlertingActions.kt
@@ -12,7 +12,7 @@ object AlertingActions {
     const val DELETE_MONITOR_ACTION_NAME = "cluster:admin/opendistro/alerting/monitor/delete"
     const val GET_FINDINGS_ACTION_NAME = "cluster:admin/opensearch/alerting/findings/get"
     const val ACKNOWLEDGE_ALERTS_ACTION_NAME = "cluster:admin/opendistro/alerting/alerts/ack"
-    const val SUBSCRIBE_FINDINGS_ACTION_NAME = "cluster:admin/opendistro/alerting/findings/subscribe"
+    const val SUBSCRIBE_FINDINGS_ACTION_NAME = "cluster:admin/opensearch/alerting/findings/subscribe"
 
     @JvmField
     val INDEX_MONITOR_ACTION_TYPE =
