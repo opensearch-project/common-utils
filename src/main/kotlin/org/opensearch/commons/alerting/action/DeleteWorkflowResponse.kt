c4e5825337d8891cd7700af2cd0467ea07ef3080
Stevan Buzejic <buzejic.stevan@gmail.com>
2023-05-15 18:42:21 +0200

    Extended deleted workflow response with list of monitor ids failed to delete (#416)
    Signed-off-by: Stevan Buzejic <buzejic.stevan@gmail.com>

--- src/main/kotlin/org/opensearch/commons/alerting/action/DeleteWorkflowResponse.kt
+++ src/main/kotlin/org/opensearch/commons/alerting/action/DeleteWorkflowResponse.kt
@@ -10,29 +10,39 @@ import org.opensearch.core.xcontent.XContentBuilder
 class DeleteWorkflowResponse : BaseResponse {
     var id: String
     var version: Long
+    var nonDeletedMonitors: List<String>? = null
 
     constructor(
         id: String,
-        version: Long
+        version: Long,
+        nonDeletedMonitors: List<String>? = null
     ) : super() {
         this.id = id
         this.version = version
+        this.nonDeletedMonitors = nonDeletedMonitors
     }
 
     constructor(sin: StreamInput) : this(
         sin.readString(), // id
-        sin.readLong() // version
+        sin.readLong(), // version
+        sin.readOptionalStringList()
     )
 
     override fun writeTo(out: StreamOutput) {
         out.writeString(id)
         out.writeLong(version)
+        out.writeOptionalStringCollection(nonDeletedMonitors)
     }
 
     override fun toXContent(builder: XContentBuilder, params: ToXContent.Params): XContentBuilder {
         return builder.startObject()
             .field(IndexUtils._ID, id)
             .field(IndexUtils._VERSION, version)
+            .field(NON_DELETED_MONITORS, nonDeletedMonitors)
             .endObject()
+    }
+
+    companion object {
+        const val NON_DELETED_MONITORS = "NON_DELETED_MONITORS"
     }
 }
