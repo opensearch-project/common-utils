b2e9ddc9aabe433901f0893c67d24fc8e3755992
Surya Sashank Nistala <snistala@amazon.com>
2023-07-10 09:36:03 -0700

    add auditDelegateMonitorAlerts flag (#476)
    * add auditDelegateMonitorAlerts flag
    
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>
    
    * add audit state check in error alert validation
    
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>
    
    * add test to verify workflow with auditDelegateMonitor flag null
    
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>
    
    ---------
    
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>

--- src/test/kotlin/org/opensearch/commons/alerting/action/GetWorkflowResponseTests.kt
+++ src/test/kotlin/org/opensearch/commons/alerting/action/GetWorkflowResponseTests.kt
@@ -4,14 +4,21 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.opensearch.common.io.stream.BytesStreamOutput
 import org.opensearch.common.io.stream.StreamInput
+import org.opensearch.commons.alerting.model.CompositeInput
+import org.opensearch.commons.alerting.model.IntervalSchedule
+import org.opensearch.commons.alerting.model.Workflow
+import org.opensearch.commons.alerting.randomDelegate
+import org.opensearch.commons.alerting.randomUser
 import org.opensearch.commons.alerting.randomWorkflow
 import org.opensearch.rest.RestStatus
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 class GetWorkflowResponseTests {
 
     @Test
-    fun testGetWorkflowRequest() {
-        val workflow = randomWorkflow()
+    fun testGetWorkflowResponse() {
+        val workflow = randomWorkflow(auditDelegateMonitorAlerts = false)
         val response = GetWorkflowResponse(
             id = "id", version = 1, seqNo = 1, primaryTerm = 1, status = RestStatus.OK, workflow = workflow
         )
@@ -20,7 +27,39 @@ class GetWorkflowResponseTests {
         val sin = StreamInput.wrap(out.bytes().toBytesRef().bytes)
         val newRes = GetWorkflowResponse(sin)
         Assertions.assertEquals("id", newRes.id)
+        Assertions.assertFalse(newRes.workflow!!.auditDelegateMonitorAlerts!!)
         Assertions.assertEquals(workflow.name, newRes.workflow!!.name)
         Assertions.assertEquals(workflow.owner, newRes.workflow!!.owner)
+    }
+
+    @Test
+    fun testGetWorkflowResponseWhereAuditDelegateMonitorAlertsFlagIsNotSet() {
+        val workflow = Workflow(
+            id = "",
+            version = Workflow.NO_VERSION,
+            name = "test",
+            enabled = true,
+            schemaVersion = 2,
+            schedule = IntervalSchedule(1, ChronoUnit.MINUTES),
+            lastUpdateTime = Instant.now(),
+            enabledTime = Instant.now(),
+            workflowType = Workflow.WorkflowType.COMPOSITE,
+            user = randomUser(),
+            inputs = listOf(CompositeInput(org.opensearch.commons.alerting.model.Sequence(listOf(randomDelegate())))),
+            owner = "",
+            triggers = listOf()
+        )
+        val response = GetWorkflowResponse(
+            id = "id", version = 1, seqNo = 1, primaryTerm = 1, status = RestStatus.OK, workflow = workflow
+        )
+        val out = BytesStreamOutput()
+        response.writeTo(out)
+        val sin = StreamInput.wrap(out.bytes().toBytesRef().bytes)
+        val newRes = GetWorkflowResponse(sin)
+        Assertions.assertEquals("id", newRes.id)
+        Assertions.assertTrue(newRes.workflow!!.auditDelegateMonitorAlerts!!)
+        Assertions.assertEquals(workflow.name, newRes.workflow!!.name)
+        Assertions.assertEquals(workflow.owner, newRes.workflow!!.owner)
+        Assertions.assertEquals(workflow.auditDelegateMonitorAlerts, newRes.workflow!!.auditDelegateMonitorAlerts)
     }
 }
