df138d2580c453e054e94148f676a65f95ccf294
bowenlan-amzn <bowenlan23@gmail.com>
2023-08-09 15:19:02 -0700

    Spotless
    Signed-off-by: bowenlan-amzn <bowenlan23@gmail.com>

--- src/test/java/org/opensearch/commons/authuser/UserTest.java
+++ src/test/java/org/opensearch/commons/authuser/UserTest.java
@@ -16,10 +16,10 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 import org.opensearch.common.io.stream.BytesStreamOutput;
+import org.opensearch.common.io.stream.StreamInput;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.core.common.Strings;
-import org.opensearch.common.io.stream.StreamInput;
 
 public class UserTest {
 
