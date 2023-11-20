6cd843d3a3e0b40af63a1075dd58ac20aa14ee48
Tyler Ohlsen <ohltyler@amazon.com>
2023-11-20 16:38:15 +0000

    Add get monitor request/response
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- src/main/kotlin/org/opensearch/commons/alerting/action/AlertingActions.kt
+++ src/main/kotlin/org/opensearch/commons/alerting/action/AlertingActions.kt
@@ -18,6 +18,7 @@ object AlertingActions {
     const val ACKNOWLEDGE_ALERTS_ACTION_NAME = "cluster:admin/opendistro/alerting/alerts/ack"
     const val ACKNOWLEDGE_CHAINED_ALERTS_ACTION_NAME = "cluster:admin/opendistro/alerting/chained_alerts/ack"
     const val SUBSCRIBE_FINDINGS_ACTION_NAME = "cluster:admin/opensearch/alerting/findings/subscribe"
+    const val GET_MONITOR_ACTION_NAME = "cluster:admin/opendistro/alerting/monitor/get"
 
     @JvmField
     val INDEX_MONITOR_ACTION_TYPE =
@@ -60,4 +61,8 @@ object AlertingActions {
     @JvmField
     val ACKNOWLEDGE_CHAINED_ALERTS_ACTION_TYPE =
         ActionType(ACKNOWLEDGE_CHAINED_ALERTS_ACTION_NAME, ::AcknowledgeAlertResponse)
+
+    @JvmField
+    val GET_MONITOR_ACTION_TYPE =
+        ActionType(GET_MONITOR_ACTION_NAME, ::GetMonitorResponse)
 }
