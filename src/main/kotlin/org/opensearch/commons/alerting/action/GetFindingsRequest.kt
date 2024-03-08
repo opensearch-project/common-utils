892c34fc1b048e43aeafdb16ad33be1a7f44651f
Riya <69919272+riysaxen-amzn@users.noreply.github.com>
2024-03-08 14:36:12 -0800

    Feature findings enhancemnt (#596)
    * added support for  param in Finding API
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * added detectionType as param for Findings API enhancements
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * adding addiional params findingIds, startTime and endTime
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * fix klint errors
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    ---------
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>

--- src/main/kotlin/org/opensearch/commons/alerting/action/GetFindingsRequest.kt
+++ src/main/kotlin/org/opensearch/commons/alerting/action/GetFindingsRequest.kt
@@ -6,6 +6,7 @@ import org.opensearch.commons.alerting.model.Table
 import org.opensearch.core.common.io.stream.StreamInput
 import org.opensearch.core.common.io.stream.StreamOutput
 import java.io.IOException
+import java.time.Instant
 
 class GetFindingsRequest : ActionRequest {
     val findingId: String?
@@ -13,19 +14,34 @@ class GetFindingsRequest : ActionRequest {
     val monitorId: String?
     val monitorIds: List<String>?
     val findingIndex: String?
+    val severity: String?
+    val detectionType: String?
+    val findingIds: List<String>?
+    val startTime: Instant?
+    val endTime: Instant?
 
     constructor(
         findingId: String?,
         table: Table,
         monitorId: String? = null,
         findingIndexName: String? = null,
-        monitorIds: List<String>? = null
+        monitorIds: List<String>? = null,
+        severity: String? = null,
+        detectionType: String? = null,
+        findingIds: List<String>? = null,
+        startTime: Instant? = null,
+        endTime: Instant? = null
     ) : super() {
         this.findingId = findingId
         this.table = table
         this.monitorId = monitorId
         this.findingIndex = findingIndexName
         this.monitorIds = monitorIds
+        this.severity = severity
+        this.detectionType = detectionType
+        this.findingIds = findingIds
+        this.startTime = startTime
+        this.endTime = endTime
     }
 
     @Throws(IOException::class)
@@ -34,7 +50,12 @@ class GetFindingsRequest : ActionRequest {
         table = Table.readFrom(sin),
         monitorId = sin.readOptionalString(),
         findingIndexName = sin.readOptionalString(),
-        monitorIds = sin.readOptionalStringList()
+        monitorIds = sin.readOptionalStringList(),
+        severity = sin.readOptionalString(),
+        detectionType = sin.readOptionalString(),
+        findingIds = sin.readOptionalStringList(),
+        startTime = sin.readOptionalInstant(),
+        endTime = sin.readOptionalInstant()
     )
 
     override fun validate(): ActionRequestValidationException? {
@@ -48,5 +69,10 @@ class GetFindingsRequest : ActionRequest {
         out.writeOptionalString(monitorId)
         out.writeOptionalString(findingIndex)
         out.writeOptionalStringCollection(monitorIds)
+        out.writeOptionalString(severity)
+        out.writeOptionalString(detectionType)
+        out.writeOptionalStringCollection(findingIds)
+        out.writeOptionalInstant(startTime)
+        out.writeOptionalInstant(endTime)
     }
 }
