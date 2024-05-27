c12827e6dd4e69fb39bb9a3497fadc4d0d748e87
Dennis Toepker <toepkerd@amazon.com>
2024-05-27 14:11:21 -0700

    added lastUpdatedTime to Note object
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>

--- src/main/kotlin/org/opensearch/commons/alerting/model/Note.kt
+++ src/main/kotlin/org/opensearch/commons/alerting/model/Note.kt
@@ -13,18 +13,16 @@ import org.opensearch.core.xcontent.XContentParser
 import org.opensearch.core.xcontent.XContentParserUtils.ensureExpectedToken
 import java.io.IOException
 import java.time.Instant
-import java.util.Collections
 import org.apache.logging.log4j.LogManager
 import org.opensearch.commons.alerting.util.optionalUserField
 import org.opensearch.commons.authuser.User
 
-private val log = LogManager.getLogger(Note::class.java)
-
 data class Note(
     val id: String = NO_ID,
-    val alertId: String = Alert.NO_ID,
+    val alertId: String = NO_ID,
     val content: String,
-    val time: Instant,
+    val createdTime: Instant,
+    val lastUpdatedTime: Instant?,
     val user: User?
 ) : Writeable, ToXContent {
 
@@ -33,7 +31,8 @@ data class Note(
         id = sin.readString(),
         alertId = sin.readString(),
         content = sin.readString(),
-        time = sin.readInstant(),
+        createdTime = sin.readInstant(),
+        lastUpdatedTime = sin.readOptionalInstant(),
         user = if (sin.readBoolean()) {
             User(sin)
         } else {
@@ -41,27 +40,39 @@ data class Note(
         }
     )
 
+    constructor(
+        alertId: String,
+        content: String,
+        createdTime: Instant,
+        user: User?
+    ) : this (
+        alertId = alertId,
+        content = content,
+        createdTime = createdTime,
+        lastUpdatedTime = null,
+        user = user
+    )
+
     @Throws(IOException::class)
     override fun writeTo(out: StreamOutput) {
         out.writeString(id)
         out.writeString(alertId)
         out.writeString(content)
-        out.writeInstant(time)
+        out.writeInstant(createdTime)
+        out.writeOptionalInstant(lastUpdatedTime)
         out.writeBoolean(user != null)
         user?.writeTo(out)
     }
 
     fun asTemplateArg(): Map<String, Any?> {
-        val templateArgMap = mutableMapOf(
+        return mapOf<String, Any?>(
             _ID to id,
             ALERT_ID_FIELD to alertId,
             NOTE_CONTENT_FIELD to content,
-            NOTE_TIME_FIELD to time
+            NOTE_CREATED_TIME_FIELD to createdTime,
+            NOTE_LAST_UPDATED_TIME_FIELD to lastUpdatedTime,
+            NOTE_USER_FIELD to user?.name
         )
-        if (user != null) {
-            templateArgMap[NOTE_USER_FIELD] = user.name
-        }
-        return Collections.unmodifiableMap(templateArgMap)
     }
 
     // used to create the Note JSON object for an API response (displayed to user)
@@ -75,11 +86,11 @@ data class Note(
     }
 
     private fun createXContentBuilder(builder: XContentBuilder, includeFullUser: Boolean): XContentBuilder {
-        log.info("include full user: $includeFullUser")
         builder.startObject()
             .field(ALERT_ID_FIELD, alertId)
             .field(NOTE_CONTENT_FIELD, content)
-            .optionalTimeField(NOTE_TIME_FIELD, time)
+            .optionalTimeField(NOTE_CREATED_TIME_FIELD, createdTime)
+            .optionalTimeField(NOTE_LAST_UPDATED_TIME_FIELD, lastUpdatedTime)
 
         if (includeFullUser) {
             // if we're storing a Note into an internal index, include full User
@@ -96,7 +107,8 @@ data class Note(
     companion object {
         const val ALERT_ID_FIELD = "alert_id"
         const val NOTE_CONTENT_FIELD = "content"
-        const val NOTE_TIME_FIELD = "time"
+        const val NOTE_CREATED_TIME_FIELD = "created_time"
+        const val NOTE_LAST_UPDATED_TIME_FIELD = "last_updated_time"
         const val NOTE_USER_FIELD = "user"
         const val NO_ID = ""
 
@@ -106,10 +118,10 @@ data class Note(
         fun parse(xcp: XContentParser, id: String = NO_ID): Note {
             lateinit var alertId: String
             var content = ""
-            lateinit var time: Instant
+            lateinit var createdTime: Instant
+            var lastUpdatedTime: Instant? = null
             var user: User? = null
 
-            log.info("note parse curr token: ${xcp.currentToken()}")
             ensureExpectedToken(XContentParser.Token.START_OBJECT, xcp.currentToken(), xcp)
             while (xcp.nextToken() != XContentParser.Token.END_OBJECT) {
                 val fieldName = xcp.currentName()
@@ -118,7 +130,8 @@ data class Note(
                 when (fieldName) {
                     ALERT_ID_FIELD -> alertId = xcp.text()
                     NOTE_CONTENT_FIELD -> content = xcp.text()
-                    NOTE_TIME_FIELD -> time = requireNotNull(xcp.instant())
+                    NOTE_CREATED_TIME_FIELD -> createdTime = requireNotNull(xcp.instant())
+                    NOTE_LAST_UPDATED_TIME_FIELD -> lastUpdatedTime = xcp.instant()
                     NOTE_USER_FIELD ->
                         user = if (xcp.currentToken() == XContentParser.Token.VALUE_NULL) {
                             null
@@ -132,7 +145,8 @@ data class Note(
                 id = id,
                 alertId = requireNotNull(alertId) { "Alert ID is null" },
                 content = content,
-                time = requireNotNull(time),
+                createdTime = createdTime,
+                lastUpdatedTime = lastUpdatedTime,
                 user = user
             )
         }
