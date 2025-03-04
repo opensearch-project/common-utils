5e086a27ef844584499fdc15d7c15c9b47ad4420
aggarwalShivani <39588384+aggarwalShivani@users.noreply.github.com>
2025-03-05 00:20:46 +0530

    Adding replication (CCR) plugin interface and classes to common-utils (#667)
    * Adding replication (CCR) plugin interface and classes
    
    Signed-off-by: aggarwalShivani <shivani.aggarwal@nokia.com>
    
    * Adding new actiontype for unfollow replication through ism plugin
    
    Signed-off-by: aggarwalShivani <shivani.aggarwal@nokia.com>
    
    * Fix ktlint issues for replication libs
    
    Signed-off-by: aggarwalShivani <shivani.aggarwal@nokia.com>
    
    * Changes for stop-replication action
    
    Signed-off-by: aggarwalShivani <shivani.aggarwal@nokia.com>
    
    * Fixed imports for AcknowledgedResponse and org.opensearch.transport classes
    
    Signed-off-by: aggarwalShivani <shivani.aggarwal@nokia.com>
    
    * Changing ReplicationPluginInterface to static object
    
    Signed-off-by: aggarwalShivani <shivani.aggarwal@nokia.com>
    
    ---------
    
    Signed-off-by: aggarwalShivani <shivani.aggarwal@nokia.com>

---
+++ src/main/kotlin/org/opensearch/commons/replication/ReplicationPluginInterface.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.commons.replication
+
+import org.opensearch.action.support.clustermanager.AcknowledgedResponse
+import org.opensearch.commons.replication.action.ReplicationActions.INTERNAL_STOP_REPLICATION_ACTION_TYPE
+import org.opensearch.commons.replication.action.StopIndexReplicationRequest
+import org.opensearch.commons.utils.recreateObject
+import org.opensearch.core.action.ActionListener
+import org.opensearch.core.action.ActionResponse
+import org.opensearch.core.common.io.stream.Writeable
+import org.opensearch.transport.client.Client
+import org.opensearch.transport.client.node.NodeClient
+
+/**
+ * Transport action plugin interfaces for the cross-cluster-replication plugin.
+ */
+object ReplicationPluginInterface {
+
+    /**
+     * Stop replication.
+     * @param client Node client for making transport action
+     * @param request The request object
+     * @param listener The listener for getting response
+     */
+
+    fun stopReplication(
+        client: Client,
+        request: StopIndexReplicationRequest,
+        listener: ActionListener<AcknowledgedResponse>
+    ) {
+        val nodeClient = client as NodeClient
+        return nodeClient.execute(
+            INTERNAL_STOP_REPLICATION_ACTION_TYPE,
+            request,
+            wrapActionListener(listener) { response ->
+                recreateObject(response) {
+                    AcknowledgedResponse(it)
+                }
+            }
+        )
+    }
+
+    /**
+     * Wrap action listener on concrete response class by a new created one on ActionResponse.
+     * This is required because the response may be loaded by different classloader across plugins.
+     * The onResponse(ActionResponse) avoids type cast exception and give a chance to recreate
+     * the response object.
+     */
+    @Suppress("UNCHECKED_CAST")
+    private fun <Response : AcknowledgedResponse> wrapActionListener(
+        listener: ActionListener<Response>,
+        recreate: (Writeable) -> Response
+    ): ActionListener<Response> {
+        return object : ActionListener<ActionResponse> {
+            override fun onResponse(response: ActionResponse) {
+                val recreated = response as? Response ?: recreate(response)
+                listener.onResponse(recreated)
+            }
+
+            override fun onFailure(exception: java.lang.Exception) {
+                listener.onFailure(exception)
+            }
+        } as ActionListener<Response>
+    }
+}
---
+++ src/main/kotlin/org/opensearch/commons/replication/action/ReplicationActions.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.commons.replication.action
+
+import org.opensearch.action.ActionType
+import org.opensearch.action.support.clustermanager.AcknowledgedResponse
+
+/**
+ * Information related to the transport stop replication action for the Replication plugin
+ */
+object ReplicationActions {
+
+    /**
+     * Action names for stopping replication
+     * STOP_REPLICATION_ACTION_NAME: action used for _replication/_stop REST API
+     * INTERNAL_STOP_REPLICATION_ACTION_NAME: Internal only - Used by Index Management plugin to invoke stop replication
+     */
+    const val STOP_REPLICATION_ACTION_NAME = "indices:admin/plugins/replication/index/stop"
+    const val INTERNAL_STOP_REPLICATION_ACTION_NAME = "indices:internal/plugins/replication/index/stop"
+
+    /**
+     * Stop replication transport action types.
+     */
+    val STOP_REPLICATION_ACTION_TYPE =
+        ActionType(STOP_REPLICATION_ACTION_NAME, ::AcknowledgedResponse)
+    val INTERNAL_STOP_REPLICATION_ACTION_TYPE =
+        ActionType(INTERNAL_STOP_REPLICATION_ACTION_NAME, ::AcknowledgedResponse)
+}
