6af306578f52f117ec988ab5a9b1eb4292d1d8b4
Shikhar Jain <8859327+shikharj05@users.noreply.github.com>
2025-03-20 13:19:44 +0530

    spotless
    Signed-off-by: Shikhar Jain <8859327+shikharj05@users.noreply.github.com>

--- src/main/java/org/opensearch/commons/authuser/User.java
+++ src/main/java/org/opensearch/commons/authuser/User.java
@@ -15,8 +15,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-
 import java.util.stream.Collectors;
+
 import org.apache.http.util.EntityUtils;
 import org.opensearch.client.Response;
 import org.opensearch.common.Nullable;
