8601c34821ce151dcba71baab86ebeb5ac06c823
Chase Engelbrecht <engechas@dev-dsk-engechas-2a-28f138b0.us-west-2.amazon.com>
2026-04-23 14:54:30 -0500

    Call initialize when providing the impl
    Signed-off-by: Chase Engelbrecht <engechas@dev-dsk-engechas-2a-28f138b0.us-west-2.amazon.com>

--- src/main/kotlin/org/opensearch/commons/utils/scheduler/SqsAccountIdProvider.kt
+++ src/main/kotlin/org/opensearch/commons/utils/scheduler/SqsAccountIdProvider.kt
@@ -1,6 +1,7 @@
 package org.opensearch.commons.utils.scheduler
 
 import org.apache.logging.log4j.LogManager
+import org.opensearch.common.settings.Settings
 import java.util.ServiceLoader
 
 /**
@@ -10,10 +11,17 @@ import java.util.ServiceLoader
  * - Have a public no-arg constructor
  * - Declare itself in META-INF/services/org.opensearch.commons.utils.scheduler.SqsAccountIdProvider
  * - Return a unique [type] string
+ * - Accept configuration via [initialize] before [getAccountIds] is called
  */
 interface SqsAccountIdProvider {
     /** Identifier used to select this provider via configuration. */
     val type: String
+
+    /**
+     * Initialize this provider with node settings. Called once after discovery
+     * and before [getAccountIds].
+     */
+    fun initialize(settings: Settings)
 
     fun getAccountIds(): List<String>
 
@@ -21,17 +29,18 @@ interface SqsAccountIdProvider {
         private val log = LogManager.getLogger(SqsAccountIdProvider::class.java)
 
         /**
-         * Find a provider matching [providerType] using the given [classLoader].
-         * Returns null if no match is found.
+         * Find a provider matching [providerType], initialize it with [settings],
+         * and return it ready to use.
          */
         @JvmStatic
-        fun find(providerType: String): SqsAccountIdProvider {
+        fun find(providerType: String, settings: Settings): SqsAccountIdProvider {
             val loader = ServiceLoader.load(SqsAccountIdProvider::class.java, SqsAccountIdProvider::class.java.classLoader)
 
             for (provider in loader) {
                 log.info("Discovered SqsAccountIdProvider: [{}]", provider.type)
                 if (provider.type == providerType) {
                     log.info("Found SqsAccountIdProvider for type [{}]", providerType)
+                    provider.initialize(settings)
                     return provider
                 }
             }
